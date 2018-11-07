19856df1f36821fbfa4427f971054de2eb2a173f
Austin <joha0033@gmail.com>
2018-11-07 14:20:15 -0700

    saga play, not good.

--- build/renderers/server.js
+++ build/renderers/server.js
@@ -12,32 +12,30 @@ var _server = require('react-dom/server');
 
 var _server2 = _interopRequireDefault(_server);
 
-var _axios = require('axios');
+var _reactRedux = require('react-redux');
 
-var _axios2 = _interopRequireDefault(_axios);
+var _store = require('../src/store');
 
-var _stateApi = require('state-api');
+var _store2 = _interopRequireDefault(_store);
 
-var _stateApi2 = _interopRequireDefault(_stateApi);
-
-var _App = require('components/App');
+var _App = require('src/components/App');
 
 var _App2 = _interopRequireDefault(_App);
-
-var _config = require('config');
-
-var _config2 = _interopRequireDefault(_config);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const serverRender = async () => {
+  const preloadedState = await _store2.default.getState();
 
-  const response = await _axios2.default.get(`http://${_config2.default.host}:${_config2.default.port}/data`);
-  const store = new _stateApi2.default(response.data);
+  const html = _server2.default.renderToString(_react2.default.createElement(
+    _reactRedux.Provider,
+    { store: _store2.default },
+    _react2.default.createElement(_App2.default, null)
+  ));
 
   return {
-    initialMarkup: _server2.default.renderToString(_react2.default.createElement(_App2.default, { store: store })),
-    initialData: response.data
+    preloadedState,
+    html
   };
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -46,28 +46,5 @@ module.exports = {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
       }
     }),
-  ],
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'async',
-  //     minSize: 30000,
-  //     maxSize: 0,
-  //     minChunks: 1,
-  //     maxAsyncRequests: 5,
-  //     maxInitialRequests: 3,
-  //     automaticNameDelimiter: '~',
-  //     name: true,
-  //     cacheGroups: {
-  //       vendors: {
-  //         test: /[\\/]vendor[\\/]/,
-  //         priority: -10
-  //       },
-  //       default: {
-  //         minChunks: 2,
-  //         priority: -20,
-  //         reuseExistingChunk: true
-  //       }
-  //     }
-  //   }
-  // }
+  ]
 }
