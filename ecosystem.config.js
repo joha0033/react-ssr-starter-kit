e48f5c0886d56f12a5d957c6c2f649b2219cd95d
Austin <joha0033@gmail.com>
2018-12-04 15:33:22 -0700

    pkgjson update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     name      : 'API',
     script    : 'app.js',
+    instances : 'max',
     env: {
       NODE_ENV: 'development'
     },
@@ -15,9 +16,9 @@ module.exports = {
       user : 'node',
       host : '212.83.163.1',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      repo : 'git@github.com:ssr-react-stater-kit.git',
       path : '/var/www/production',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
-};
+}
